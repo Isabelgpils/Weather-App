80d6478bf1aabcee6e0ad189056b897c2092f2fc
Isabel Gutierrez-Pils <isabelgpils@gmail.com>
2019-04-17 09:08:05 -0500

    edits to use heroku

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 
-
+const port = process.env.PORT || 3000
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -103,7 +103,7 @@ app.get('*', (req, res)=>{
      })
 })
 // need to start browser - only used one time
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     //callback that runs when server is up and running
-    console.log('Server is up on port 3000')
+    console.log('Server is up on port PORT '+ port )
 })
