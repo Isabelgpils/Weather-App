8dfc4534b60f30f586d1b446d176ab996d20ab85
Isabel Gutierrez-Pils <isabelgpils@gmail.com>
2019-04-18 16:06:29 -0500

    Added low and hig temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,5 @@
 const request = require('request')
 
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
- 
 const forecast = (longitude, latitude, callback)=>{
 
 const url = 'https://api.darksky.net/forecast/9023c1bf0e8ff37154844560daca49d3/' + longitude + ',' + latitude +'?lang=en'
@@ -21,7 +11,7 @@ const url = 'https://api.darksky.net/forecast/9023c1bf0e8ff37154844560daca49d3/'
      callback('Unable to find location. Try another search', undefined)
    }else{
      callback(undefined, 
-        response.body.daily.data[1].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a '  + response.body.currently.precipProbability + '% chance of rain.')
+        response.body.daily.data[1].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. The high is ' + response.body.daily.data[0].temperatureHigh + ' and the low is ' + response.body.daily.data[0].temperatureLow + '. There is a '  + response.body.currently.precipProbability + '% chance of rain.')
      }
 })
 }
